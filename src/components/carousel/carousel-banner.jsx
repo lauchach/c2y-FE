4abd5b980e0871c6d35ce398859f18141183699f
lauchach <tarnapot.b38@gmail.com>
2024-02-04 11:11:36 +0700

    [edit] update bord

--- src/components/carousel/carousel-banner.jsx
+++ src/components/carousel/carousel-banner.jsx
@@ -3,7 +3,11 @@ import styles from './carousel-banner.module.scss'
 const carouselBanner = () => {
   return (
     <>
-      <div className={`${styles.bannerDesktop} rounded-2xl`}></div>
+      <div className={`${styles.bannerDesktop} rounded-2xl`}>
+        <div className={styles.bordImg}>
+          <img src={`src/assets/brodimg.jpg`}/>
+        </div>  
+      </div>
       <div className={`${styles.bannerMobile} rounded-2xl`}></div>
     </>
   )
